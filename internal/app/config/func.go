0f1554c3954e5a69baa801dc8802aa46ab5114a6
千橘 雫霞 <qianjunakasumi@gmail.com>
2021-02-08 23:36:38 +0800

    史诗：设置设备信息

--- internal/app/config/func.go
+++ internal/app/config/func.go
@@ -3,25 +3,66 @@ package config
 import (
 	"github.com/qianjunakasumi/Konoe/internal/pkg/logger"
 
+	"github.com/Mrs4s/MiraiGo/client"
 	"go.uber.org/zap"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 )
 
 // GetClientConfig 获取客户端配置
-func GetClientConfig() (conf *Client, err error) {
+func GetClientConfig() (c *Client, err error) {
 
 	f, err := ioutil.ReadFile(".konoe/config.yml")
 	if err != nil {
-		logger.Error("无法读取配置文件", zap.Error(err))
-		return nil, err
+		logger.Error("无法读取客户端配置文件", zap.Error(err))
+		return
 	}
 
-	err = yaml.Unmarshal(f, &conf)
+	err = yaml.Unmarshal(f, &c)
 	if err != nil {
-		logger.Error("无法解析配置文件或损坏的配置文件", zap.Error(err))
-		return nil, err
+		logger.Error("无法解析配置文件或损坏的客户端配置文件", zap.Error(err))
+		return
 	}
 
 	return
+}
+
+// SetDeviceConfig 设置设备配置
+func SetDeviceConfig() (err error) {
+
+	var c *Device
+
+	f, err := ioutil.ReadFile(".konoe/device.yml")
+	if err != nil {
+		logger.Error("无法读取设备配置文件", zap.Error(err))
+		return
+	}
+
+	err = yaml.Unmarshal(f, &c)
+	if err != nil {
+		logger.Error("无法解析配置文件或损坏的客户端配置文件", zap.Error(err))
+		return
+	}
+
+	setDeviceConfig(c)
+	return
+}
+
+// setDeviceConfig 设置设备配置
+func setDeviceConfig(c *Device) {
+	client.SystemDeviceInfo.Protocol = client.AndroidPhone
+
+	client.SystemDeviceInfo.Display = []byte(c.Display)
+	client.SystemDeviceInfo.Model = []byte(c.Model)
+	client.SystemDeviceInfo.Brand = []byte(c.Brand)
+	client.SystemDeviceInfo.Board = []byte(c.Board)
+	client.SystemDeviceInfo.Device = []byte(c.Device)
+	client.SystemDeviceInfo.Product = []byte(c.Product)
+
+	client.SystemDeviceInfo.FingerPrint = []byte(c.Fingerprint)
+	client.SystemDeviceInfo.ProcVersion = []byte(c.Kernel)
+	client.SystemDeviceInfo.SimInfo = []byte(c.SIM)
+	client.SystemDeviceInfo.MacAddress = []byte(c.MAC)
+	client.SystemDeviceInfo.IpAddress = []byte(c.IP)
+	client.SystemDeviceInfo.IMEI = c.IMEI
 }
--- internal/app/config/struct.go
+++ internal/app/config/struct.go
@@ -1,7 +1,25 @@
 package config
 
-// Client 客户端
-type Client struct {
-	Number   uint64 `yaml:"number"`   // 号码
-	Password string `yaml:"password"` // 密码
-}
+type (
+	// Client 客户端
+	Client struct {
+		Number   uint64 `yaml:"number"`   // 号码
+		Password string `yaml:"password"` // 密码
+	}
+
+	// Device 设备
+	Device struct {
+		Display     string `yaml:"display"`     // 显示
+		Model       string `yaml:"model"`       // 型号
+		Brand       string `yaml:"brand"`       // 制造商
+		Board       string `yaml:"board"`       // 主板
+		Device      string `yaml:"device"`      // 设备
+		Product     string `yaml:"product"`     // 产品
+		Fingerprint string `yaml:"fingerprint"` // 编译序列
+		Kernel      string `yaml:"kernel"`      // 内核版本
+		SIM         string `yaml:"sim"`         // 运营商信息
+		MAC         string `yaml:"mac"`         // MAC 地址
+		IP          string `yaml:"ip"`          // IP 地址
+		IMEI        string `yaml:"imei"`        // IMEI
+	}
+)
